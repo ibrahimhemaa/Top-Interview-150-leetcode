86d25eb05830c44e4617793bffa87c3742d1f4aa
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2024-08-20 15:52:54 +0300

    Time: 22 ms (44.07%) | Memory: 29 MB (5.75%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -6,6 +6,6 @@ public:
         k%=n;
         for(int i=n-k;i<n;i++)re.push_back(nums[i]);
         for(int i=0;i<n-k;i++)re.push_back(nums[i]);
-        for(int i=0;i<n;i++)nums[i]=re[i];
+        nums=re;
     }
 };
