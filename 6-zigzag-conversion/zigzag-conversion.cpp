330471a661205859bf976be67f3f0e4ba9c0f036
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2024-08-24 08:29:15 +0300

    Time: 14 ms (41.38%) | Memory: 12.5 MB (75.78%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -2,23 +2,13 @@ class Solution {
 public:
     string convert(string s, int numRows) {
         vector<string>s1(numRows);
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            int j=i;
-            //top to botton
-            while(j<n&&j-i<numRows){
-                s1[j-i].push_back(s[j]);
-                j++;
-            }
-            int j1=j;
-            j-=2;
-            int cnt=0;
-            //zigza,g botton to top
-            while(cnt+j1<n &&j>i){
-                s1[j-i].push_back(s[j1+cnt]);
-                j--;cnt++;
-            }
-            i=j1+cnt-1;
+        int i=0,add=1;
+        if(numRows==1)return s;
+        for(auto &j:s){
+            s1[i].push_back(j);
+            if(i==numRows-1 || (i==0 &&!~add))add*=-1;
+            i+=add;
+            
         }
         s.clear();
         for(auto &i:s1)s+=i;
