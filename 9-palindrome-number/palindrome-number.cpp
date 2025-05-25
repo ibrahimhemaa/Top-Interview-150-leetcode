ab7509952ae9797990104d946a39212d1b44e548
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2025-05-25 23:09:02 +0300

    Time: 3 ms (46.90%) | Memory: 10.9 MB (16.82%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string s=to_string(x);
-        int n=s.size();
-        for(int i=0;i<n/2;i++){
-            if(s[i]!=s[n-i-1]){return false;}
-        }
-        return true;
+        string s=to_string(x),s1=s;
+        reverse(s1.begin(),s1.end());
+        return s==s1;
     }
 };
