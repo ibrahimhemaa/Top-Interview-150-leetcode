eedd51bcd281ee2c7e7cc717d8d198a513b865b9
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2025-06-04 02:11:48 +0300

    Time: 238 ms (5.05%) | Memory: 103.1 MB (5.05%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    set<vector<int>>ans;
+    void solve(vector<int>candidates,int target,int sum=0,vector<int>v={}){
+        if(sum>=target){
+            if(sum==target){
+                sort(v.begin(),v.end());
+
+                ans.insert(v);
+            }
+            return;
+        }
+        for(int i=0;i<candidates.size();i++){
+            if(candidates[i]<=target-sum){
+                v.push_back(candidates[i]);
+                solve(candidates,target,sum+candidates[i],v);
+                v.pop_back();
+            }
+            else{
+                break;
+            }
+        }
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        solve(candidates,target);
+        vector<vector<int>>v;
+        for(auto x:ans)v.push_back(x);
+        return v;
+    }
+};
