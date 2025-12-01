c4ee5bb5baf27d88a78a177c6f6e55c168004f26
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2025-12-01 21:47:43 +0200

    Time: 49 ms (10.36%) | Memory: 59.7 MB (59.22%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public: 
+    using ll=long long;
+    long long maxRunTime(int n, vector<int>& batteries) {
+        sort(batteries.begin(),batteries.end());
+        ll l=1,r=1e14;
+        //may be my solution memories but this is best solution 
+        while(l<=r){
+            ll mid=(l+r)>>1;
+            int cnt=0;
+            ll sum=0;
+            for(ll i:batteries){
+                sum+=min(i,mid);
+            }
+            if(sum/mid>=n){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        } 
+        return l-1;
+    }
+};
