8788f644a69f44cffa647d115425acebe5b6cbc2
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2025-12-08 07:21:22 +0200

    Time: 8 ms (50.66%) | Memory: 7.9 MB (55.75%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -4,8 +4,7 @@ public:
         int cnt=0;
         for(int i=1;i<=n;i++){
             for(int j=i+1;j<=n;j++){
-                int val=j*j-i*i;
-                if(sqrt(val)==ceil(sqrt(val)))++cnt;
+                cnt+=sqrt(j*j-i*i)==ceil(sqrt(j*j-i*i));
             }
         }
         return cnt;
