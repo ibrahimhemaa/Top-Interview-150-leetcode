c4b8909bed8e991bf496b3caebf8e736e52d9af7
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2025-12-09 21:41:08 +0200

    Time: 0 ms (100.00%) | Memory: 8.4 MB (12.59%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int hammingWeight(int n) {
         int cnt=0;
-        for(int i=0;i<31;i++){
-            cnt+=((n & (1<<i))>0);
+        while(n){
+            if(n&1)++cnt;
+            n>>=1;
         }
         return cnt;
     }
