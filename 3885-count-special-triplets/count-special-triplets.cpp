db582e95d5b6e452cfca8b9906f8f3285de580b7
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2025-12-09 12:29:54 +0200

    Time: 327 ms (95.26%) | Memory: 280.3 MB (95.62%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     // using s=second;
     int specialTriplets(vector<int>& nums) {
-        unordered_map<int,int>right,left;
-        for(auto &i:nums)right[i]++;
+        vector<unordered_map<int, int>> cnt(2);
         int ans=0;
         for(auto &i:nums){
-            right[i]--;
-            int need=2*i;
-            ans=(ans+(1ll*right[need]*left[need])%1000000007)%1000000007;
-            left[i]++;
+            if(i%2==0 && cnt[1].count(i/2)){
+                ans=(ans+cnt[1][i/2]) % 1000000007;
+            }
+            if (cnt[0].count(2 *i)) {
+                cnt[1][i] = (cnt[1][i] + cnt[0][2 * i]) % 1000000007;
+            }
+            cnt[0][i]++;
         }
+        
         return ans;
     }
 };
