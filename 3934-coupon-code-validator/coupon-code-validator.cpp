499e046f5e2e69401e13b6478a528277fcbaa871
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2025-12-13 13:43:39 +0200

    Time: 3 ms (81.55%) | Memory: 68.5 MB (81.54%) - LeetSync

--- 3934-coupon-code-validator/coupon-code-validator.cpp
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -2,12 +2,7 @@ class Solution {
 public:
     vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
         
-        map<char,int>businessHash;
-        businessHash['e']=1;
-        businessHash['g']=2;
-        businessHash['p']=3;
-        businessHash['r']=4;
-        vector<pair<int,string>>category;
+        vector<pair<char,string>>category;
         int n=code.size();
         for(int i=0;i<n;i++){
             if(!isActive[i]||(businessLine[i]!="electronics"&&businessLine[i]!="grocery"&&businessLine[i]!="pharmacy"&&businessLine[i]!="restaurant") )continue;
@@ -17,12 +12,12 @@ public:
                 validword&=((code[i][j]>='0'&&code[i][j]<='9')||(code[i][j]>='a'&&code[i][j]<='z')||(code[i][j]>='A'&&code[i][j]<='Z')||(code[i][j]=='_'));
             }
             if(validword){
-                category.push_back({businessHash[businessLine[i][0]],code[i]});
+                category.push_back({businessLine[i][0],code[i]});
             }
         }
         sort(category.begin(),category.end());
         vector<string>Category;
-        for(auto &[hash,code]:category)Category.push_back(code);
+        for(auto &[_,code]:category)Category.push_back(code);
         return Category;
     }
 };
