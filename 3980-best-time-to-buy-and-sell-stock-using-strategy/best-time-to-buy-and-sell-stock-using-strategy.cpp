94a5cb5e305e7fa484bd785ea6adfaeef8c2f520
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2025-12-18 11:02:33 +0200

    Time: 17 ms (38.12%) | Memory: 316.1 MB (52.22%) - LeetSync

--- 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.cpp
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -3,18 +3,22 @@ public:
     using ll=long long;
     long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
         int n=prices.size();
-        vector<vector<ll>>pref(n,vector<ll>(2,0));
+        vector<ll>pref(n,0);
         for(int i=0;i<n;i++){
-            pref[i][0]=1ll*strategy[i]*prices[i];//cur pref
-            pref[i][1]=prices[i];//sell pref
+            pref[i]=1ll*strategy[i]*prices[i];
             if(i){
-                pref[i][0]+=pref[i-1][0];
-                pref[i][1]+=pref[i-1][1];
+                pref[i]+=pref[i-1];
             }
         }
-        ll ans=pref[n-1][0];
-        for(int i=k-1;i<n;i++){
-            ans=max(ans,pref[n-1][0]-pref[i][0]+(i-k>-1?pref[i-k][0]:0)+pref[i][1]-pref[i-k/2][1]);
+        ll ans=pref[n-1];
+        ll cur1=0;
+        for(int i=k/2;i<n;i++){
+            cur1+=prices[i];
+            if(i>=k-1){//2 3 
+                if(i>k-1)cur1-=prices[i-k/2];
+                ans=max(ans,pref[n-1]-pref[i]+(i-k>-1?pref[i-k]:0)+cur1);
+            }
+            
         }
         return ans;
     }
