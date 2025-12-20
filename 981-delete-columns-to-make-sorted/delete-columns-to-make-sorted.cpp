ed88925cab1dd3ea7376a303288a2f86c98f83e9
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2025-12-20 06:48:31 +0200

    Time: 3 ms (63.02%) | Memory: 15.6 MB (14.42%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
@@ -4,8 +4,8 @@ public:
         int number_of_Column=0;
         int n=strs.size(),m=strs[0].size();
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(j && strs[j][i]<strs[j-1][i]){
+            for(int j=1;j<n;j++){
+                if(strs[j][i]<strs[j-1][i]){
                     ++number_of_Column;break;
                 }
             }
