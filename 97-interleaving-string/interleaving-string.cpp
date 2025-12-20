59120972938e0996fc6813200cff75f6fa041451
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2025-12-20 09:37:40 +0200

    Time: 0 ms (100.00%) | Memory: 9.8 MB (36.01%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -16,13 +16,10 @@ public:
         return ret;
     }
     bool isInterleave(string s1, string s2, string s3) {
-        s1="#"+s1;
-        s2="#"+s2;
-        s3="##"+s3;
         int n=s1.size(),m=s2.size(),k=s3.size();
         if(n+m!=k)return 0;
         dp.resize(n+2,vector<int>(m+2,-1));
-        return (isInnterval(0,1,s1,s2,s3,n,m,k)|isInnterval(1,0,s1,s2,s3,n,m,k));
+        return (isInnterval(0,0,s1,s2,s3,n,m,k));
 
     }
 };
