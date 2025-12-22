1d9bf47500998129bf79accd3f540b208bc9707c
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2025-12-22 18:04:23 +0200

    Time: 40 ms (5.13%) | Memory: 22 MB (5.13%) - LeetSync

--- 1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.cpp
+++ 1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.cpp
@@ -10,11 +10,6 @@ public:
         ret=max(ret,maxlexi(strs,m,i+1,prev));
         //pick
         if(~prev){
-            // bool validindx=1;
-            // for(string word:strs){100*100*100
-            //     validindx&=(word[prev]<=word[i]);
-            // }
-            cout<<i<<' '<<prev+1<<' '<<valid[i][prev+1]<<'\n';
             if(valid[i][prev])ret=max(ret,1+maxlexi(strs,m,i+1,i));
         }
         else{
@@ -28,16 +23,14 @@ public:
         dp.resize(m+1,vector<int>(m+2,-1));
         valid.resize(m+1,vector<int>(m+2,0));
 
-        // for(auto &word:strs){
-            for(int c=0;c<m;c++){
-                for(int nexc=c+1;nexc<m;nexc++){
-                    int validstep=1;
-                    for(auto &word:strs)validstep&=(word[c]<=word[nexc]);
-                    // cout<<c<<' '<<nexc<<' '<<validstep<<'\n';
-                    valid[nexc][c]=validstep;
-                }
+        for(int c=0;c<m;c++){
+            for(int nexc=c+1;nexc<m;nexc++){
+                int validstep=1;
+                for(auto &word:strs)validstep&=(word[c]<=word[nexc]);
+                valid[nexc][c]=validstep;
             }
-        // }
+        }
+        
         return m-maxlexi(strs,m,0,-1);
     }
 };
