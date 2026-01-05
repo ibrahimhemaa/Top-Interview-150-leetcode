31c5b99464a0212d968985823d08a4045f9fdb3a
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2026-01-05 18:42:19 +0200

    Time: 3 ms (46.23%) | Memory: 43.8 MB (7.55%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
         long long sum=0;
-        int cnt=0,zero=0,mn=1e6;
+        int cnt=0,mn=1e6;
         for(auto mat:matrix){
-            for(int num:mat){sum+=abs(num);
-            zero+=(num==0);
-            mn=min(mn,abs(num));cnt+=(num<0);}
+            for(int num:mat){
+                sum+=abs(num);
+                mn=min(mn,abs(num));
+                cnt+=(num<0);
+            }
         }
-        if(cnt%2&&!zero)return sum-2*mn;
-        else return sum;
+        if(cnt%2==0)mn=0;
+        return sum-2*mn;
+       
     }
 };
