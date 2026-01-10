69ba6822231217369b438e4bd796ad24a24c68bc
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2026-01-10 14:38:19 +0200

    Time: 37 ms (23.87%) | Memory: 18.4 MB (23.97%) - LeetSync

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>>mnsum;
+    int sol(int i,int j,int &n,int &m,string &s1,string &s2){
+        if(i>=n&&j>=m)return 0;
+        if(i==n||j==m){
+            int ans=0;
+            while(i!=n)ans+=97+(s1[i++]-'a');
+            while(j!=m)ans+=97+(s2[j++]-'a');
+            return ans;
+        }
+        int &ret=mnsum[i][j];
+        if(~ret)return ret;
+        ret=1000000;
+        ret=min(ret,sol(i+1,j,n,m,s1,s2)+(97+(s1[i]-'a')));
+        ret=min(ret,sol(i,j+1,n,m,s1,s2)+(97+(s2[j]-'a')));
+        if(s1[i]==s2[j]){
+            ret=min(ret,sol(i+1,j+1,n,m,s1,s2));
+        }
+        return ret;
+    }
+    int minimumDeleteSum(string s1, string s2) {
+        int n=s1.size(),m=s2.size();
+        mnsum.resize(n,vector<int>(m,-1));
+        return sol(0,0,n,m,s1,s2);
+    }
+};
