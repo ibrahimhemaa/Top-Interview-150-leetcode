0d19d6bf8fbdc24a92f6a276f8ce65e07f87cd95
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2026-01-10 14:48:29 +0200

    Time: 27 ms (37.69%) | Memory: 18.5 MB (9.87%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<vector<int>>mnsum;
+    vector<int>suff1,suff2;
     int sol(int i,int j,int &n,int &m,string &s1,string &s2){
         if(i>=n&&j>=m)return 0;
-        if(i==n||j==m){
-            int ans=((m-i+n-j)*97);
-            while(i!=n)ans+=(s1[i++]-'a');
-            while(j!=m)ans+=(s2[j++]-'a');
-            return ans;
-        }
+        if(i==n||j==m)return ((m-i+n-j)*97)+suff1[i]+suff2[j];
+        
         int &ret=mnsum[i][j];
         if(~ret)return ret;
         ret=sol(i+1,j,n,m,s1,s2)+(97+(s1[i]-'a'));
@@ -21,6 +18,10 @@ public:
     int minimumDeleteSum(string s1, string s2) {
         int n=s1.size(),m=s2.size();
         mnsum.resize(n,vector<int>(m,-1));
+        suff1.resize(n+1,0);
+        suff2.resize(m+1,0);
+        for(int i=n-1;i>=0;i--)suff1[i]=suff1[i+1]+(s1[i]-'a');
+        for(int i=m-1;i>=0;i--)suff2[i]=suff2[i+1]+(s2[i]-'a');
         return sol(0,0,n,m,s1,s2);
     }
 };
